232161b2f7184236eebd3636c6a51236d36e24da
Michał Lipowski <m.lipowski95@gmail.com>
2020-03-26 22:41:52 +0100

    API crud changes Create generic model for crud operations and user it for existings schemas
    Changelog:
    * Added crud/base.py for generic CRUD operations
    * Added crud/crud_user.py for user specific operations
    * Changed User schema to handle endpoint specific properties
    * Added User password hashing via JWT and storing hashed password in database
    * Updated requirements.txt

--- app/api/user.py
+++ app/api/user.py
@@ -1,42 +1,94 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Body
+from fastapi.encoders import jsonable_encoder
+from pydantic import EmailStr
 from sqlalchemy.orm import Session
+from app import crud
 
 from app.api.utils.db import get_db
-from app.models.user import User
-from app.schemas.user import UserBase
+from app.models.user import User as DBUser
+from app.schemas.user import User, UserCreate, UserUpdate
 
 router = APIRouter()
 
 
-@router.post("/", response_model=UserBase)
-async def create_user(user: UserBase,
-                      db: Session = Depends(get_db)):
-    """
-    Create new user.
-    """
-    db_user = User(
-        first_name=user.first_name, last_name=user.last_name, email=user.email,
-        is_active=user.is_active, is_superuser=user.is_superuser
-    )
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
-
-
-@router.get("/", response_model=List[UserBase])
-async def read_users(db: Session = Depends(get_db)):
+@router.get("/", response_model=List[User])
+def read_users(
+        db: Session = Depends(get_db),
+        skip: int = 0,
+        limit: int = 100,
+):
     """
     Retrieve users.
     """
-    return db.query(User).all()
+    users = crud.user.get_multi(db, skip=skip, limit=limit)
+    return users
 
 
-@router.get("/{user_id}", response_model=UserBase)
-def read_user_by_id(user_id: int,
-                    db: Session = Depends(get_db)):
+@router.post("/", response_model=User)
+def create_user(
+        *,
+        db: Session = Depends(get_db),
+        user_in: UserCreate
+):
+    """
+    Create new user.
+    """
+    user = crud.user.get_by_email(db, email=user_in.email)
+    if user:
+        raise HTTPException(
+            status_code=400,
+            detail="The user with this email already exists in the system.",
+        )
+    user = crud.user.create(db, obj_in=user_in)
+    return user
+
+
+@router.get("/{user_id}", response_model=User)
+def read_user_by_id(
+        user_id: int,
+        db: Session = Depends(get_db)
+):
     """
     Get a specific user by id.
     """
-    return db.query(User).filter(user_id == User.id).first()
+    user = crud.user.get(db, id=user_id)
+    return user
+
+
+@router.put("/{user_id}", response_model=User)
+def update_user(
+        *,
+        db: Session = Depends(get_db),
+        user_id: int,
+        user_in: UserUpdate
+):
+    """
+    Update a user.
+    """
+    user = crud.user.get(db, id=user_id)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="The user does not exist in the system",
+        )
+    user = crud.user.update(db, db_obj=user, obj_in=user_in)
+    return user
+
+
+@router.delete("/{user_id}", response_model=User)
+def remove_user_by_id(
+        user_id: int,
+        db: Session = Depends(get_db)
+):
+    """
+    Remove a specific user by id
+    """
+    user = crud.user.get(db, id=user_id)
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="The user does not exist in the system",
+        )
+    user = crud.user.remove(db, id=user_id)
+    return user
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,15 +1,39 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class UserBase(BaseModel):
-    id: int
-    email: str
-    first_name: str
+    email: Optional[EmailStr]
+    first_name: Optional[str]
     last_name: Optional[str]
     is_active: Optional[bool] = True
     is_superuser: Optional[bool] = False
 
+
+class UserBaseInDB(UserBase):
+    id: int = None
+
     class Config:
         orm_mode = True
+
+
+# Properties to receive via API on creation
+class UserCreate(UserBaseInDB):
+    email: EmailStr
+    password: str
+
+
+# Properties to receive via API on update
+class UserUpdate(UserBaseInDB):
+    password: Optional[str] = None
+
+
+# Additional properties to return via API
+class User(UserBaseInDB):
+    pass
+
+
+# Additional properties stored in DB
+class UserInDB(UserBaseInDB):
+    hashed_password: str
