3e4cdd565048ba48c5a32447f8d357762dc0888e
lipowskm <m.lipowski95@gmail.com>
2020-03-29 18:41:20 +0200

    add X-process-time response header to track response times

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
+import time
+from urllib.request import Request
+
 import uvicorn
 from fastapi import FastAPI
 
 from app import init_data
 from app.api.api import api_router
 from app.core import config
+from app.init_data import logger
 
 init_data.init()
-app = FastAPI()
+app = FastAPI(title='Simple Workout Tracker API')
 app.include_router(api_router, prefix="/api")
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 if __name__ == "__main__":
