7b7ef030493b97c2a1aae6ee285511f582e62d7c
lipowskm <m.lipowski95@gmail.com>
2020-03-31 14:29:34 +0200

    Update CRUD documentation

--- app/crud/base.py
+++ app/crud/base.py
@@ -23,20 +23,42 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         self.model = model
 
     async def get(self, id: int) -> Record:
+        """
+        Return single record from given object ID.
+        :param id: Database object id.
+        :return: asyncpg Record object containing data.
+        """
         query = self.model.__table__.select().where(id == self.model.id)
         return await database.fetch_one(query=query)
 
     async def get_multi(self, skip=0, limit=100) -> List[Record]:
+        """
+        Return list of desired objects.
+        :param skip: How many of first objects to skip in the result.
+        :param limit: Max number of objects to get from query.
+        :return: List of asyncpg Record objects containing data.
+        """
         query = self.model.__table__.select().offset(skip).limit(limit)
         return await database.fetch_all(query=query)
 
     async def create(self, obj_in: CreateSchemaType) -> int:
+        """
+        Create object in database.
+        :param obj_in: Class inheriting from pydantic BaseModel with data needed for object creation.
+        :return: id of created object.
+        """
         obj_in_data = jsonable_encoder(obj_in)
         db_obj = self.model(**obj_in_data)
         query = self.model.__table__.insert().values(**db_obj)
         return await database.execute(query=query)
 
     async def update(self, id: int, obj_in: UpdateSchemaType) -> int:
+        """
+        Update object with given id.
+        :param id: id of updated object in database.
+        :param obj_in: Class inheriting from pydantic BaseModel with data needed for object update.
+        :return: id of updated object in database.
+        """
         obj_in_data = jsonable_encoder(obj_in)
         db_obj = self.model(**obj_in_data)
         query = (
@@ -44,6 +66,11 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         )
         return await database.execute(query=query)
 
-    async def remove(self, id: int) -> None:
-        query = self.model.__table__.delete().where(id == self.model.id)
+    async def remove(self, id: int) -> int:
+        """
+        Remove object with given id.
+        :param id: id of object in database.
+        :return: id of object in database.
+        """
+        query = self.model.__table__.delete().where(id == self.model.id).returning(self.model.id)
         return await database.execute(query=query)
